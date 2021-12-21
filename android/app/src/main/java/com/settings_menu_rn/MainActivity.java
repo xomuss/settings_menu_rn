8e9ab1ffab1daf37884169a2e2d5324427544a3b
xomuss <olexandr.khomenko88@gmail.com>
2021-12-21 21:31:32 +0200

    start

---
+++ android/app/src/main/java/com/settings_menu_rn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.settings_menu_rn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "settings_menu_rn";
+  }
+}
